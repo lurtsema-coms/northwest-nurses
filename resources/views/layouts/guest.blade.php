d1e3e05d1542bac8546c2ff896d809a8b70ed045
GabQuing <gab.quing29@gmail.com>
2024-05-30 01:04:09 +0800

    in progress login page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,16 +14,15 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+    <body class="font-sans text-gray-900 antialiased flex m-auto min-h-screen">
+        <div class="w-full max-w-7xl flex flex-row justify-center m-auto bg-white shadow-md overflow-hidden h-[35rem]">
+            <div class=" w-full flex-1 px-14 py-4 m-auto ">
                 {{ $slot }}
+            </div>
+            <div class="w-full flex-1  bg-primary">
+                <div>
+                    <span>dajskldasjdlksajkld</span>
+                </div>
             </div>
         </div>
     </body>
