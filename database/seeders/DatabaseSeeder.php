93d9de8bb73b793c117163c8257280b1bceae525
Fil <filongunio@gmail.com>
2024-06-01 05:33:35 +0800

    create seeder for user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,18 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'email' => 'test@employer.com',
+            'contact_number' => '1234567890',
+            'address' => '123 Employer St, Employertown, CA 90210',
+            'role' => 'employer',
+        ]);
+
+        \App\Models\User::factory()->create([
+            'email' => 'test@applicant.com',
+            'contact_number' => '1234567890',
+            'address' => '123 Applicant St, Applicantown, CA 90210',
+            'role' => 'applicant',
+        ]);
     }
 }
