cb69c56d91af6ebf5e75416656baa1d4e0450c91
Patwicku <punzalan2233@gmail.com>
2024-06-04 02:37:42 +0800

    auth session

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -25,11 +26,18 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
+        $email = $request->get('email');
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        $user = User::where('email', $email)->first();
+
+        if($user->role == 'employer'){
+            return redirect()->intended('/employer-dashboard');
+        }else{
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }
     }
 
     /**
--- resources/views/components/employer/emp-profile.blade.php
+++ resources/views/components/employer/emp-profile.blade.php
@@ -1,3 +1,50 @@
-<div class="bg-red-300">
-    <p>hey</p>
+<div class="bg-white mx-auto max-w-3xl p-10 rounded-xl shadow-sm text-slate-600">
+    <p class="font-bold mb-5">Edit</p>
+    <form action="">
+        <div class="space-y-5 text-slate-500">
+            <div class="flex flex-wrap gap-5">
+                <div class="flex flex-col flex-1 space-y-2">
+                    <span class="font-medium">First Name</span>
+                    <input class="h-10 text-sm px-2 border border-gray-300 rounded-md focus:border-1 focus:border-cyan-600 focus:ring-0 focus:outline-none" type="text" required>
+                </div>
+                <div class="flex flex-col flex-1 space-y-2">
+                    <span class="font-medium">Last Name</span>
+                    <input class="h-10 text-sm px-2 border border-gray-300 rounded-md focus:border-1 focus:border-cyan-600 focus:ring-0 focus:outline-none" type="text" required>
+                </div>
+            </div>
+            <div class="flex flex-wrap gap-5">
+                <div class="flex flex-col flex-1 space-y-2">
+                    <span class="font-medium">Contact Number</span>
+                    <input class="h-10 text-sm px-2 border border-gray-300 rounded-md focus:border-1 focus:border-cyan-600 focus:ring-0 focus:outline-none" type="text" required>
+                </div>
+                <div class="flex flex-col flex-1 space-y-2">
+                    <span class="font-medium">Email</span>
+                    <input class="h-10 text-sm px-2 border border-gray-300 rounded-md focus:border-1 focus:border-cyan-600 focus:ring-0 focus:outline-none" type="text" required>
+                </div>
+            </div>
+            <div class="flex flex-wrap gap-5">
+                <div class="flex flex-col flex-1 space-y-2">
+                    <span class="font-medium">Home Address</span>
+                    <input class="h-10 text-sm px-2 border border-gray-300 rounded-md focus:border-1 focus:border-cyan-600 focus:ring-0 focus:outline-none" type="text" required>
+                </div>
+                <div class="flex flex-col space-y-2">
+                    <span class="font-medium">Birth Date</span>
+                    <input class="h-10 text-sm px-2 border border-gray-300 rounded-md focus:border-1 focus:border-cyan-600 focus:ring-0 focus:outline-none w-48" type="date" required>
+                </div>
+            </div>
+            <div class="flex flex-wrap gap-5">
+                <div class="flex flex-col flex-1 space-y-2">
+                    <span class="font-medium">Change Password</span>
+                    <input class="h-10 text-sm px-2 border border-gray-300 rounded-md focus:border-1 focus:border-cyan-600 focus:ring-0 focus:outline-none" type="text" required>
+                </div>
+                <div class="flex flex-col flex-1 space-y-2">
+                    <span class="font-medium">Confirm Password</span>
+                    <input class="h-10 text-sm px-2 border border-gray-300 rounded-md focus:border-1 focus:border-cyan-600 focus:ring-0 focus:outline-none" type="password">
+                </div>
+            </div>
+            <div class="text-end">
+                <button class="bg-cyan-600 text-white text-sm border h-10 text-sm px-4 rounded-md hover:opacity-70">Save</button>
+            </div>
+        </div>
+    </form>
 </div>
