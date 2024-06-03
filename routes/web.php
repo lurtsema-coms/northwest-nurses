53cefca47eb60f672022554be28f766d3e52b4c8
Fil <filongunio@gmail.com>
2024-06-04 03:01:24 +0800

    fix typo verified

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::get('/job-info', function () {
 });
 
 
-Route::middleware(['auth', 'verfied'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
