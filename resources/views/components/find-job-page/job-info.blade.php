97d9ad32278ceaefc3069bad02a6fdac53bbd753
Fil <filongunio@gmail.com>
2024-07-24 02:00:33 +0800

    adjust width size of job info

--- resources/views/components/find-job-page/job-info.blade.php
+++ resources/views/components/find-job-page/job-info.blade.php
@@ -1,13 +1,13 @@
-<div class="sticky flex flex-col justify-start items-center max-h-dvh top-0 mx-auto max-w-screen-md flex-grow bg-white border-2 border-gray-300 rounded-xl overflow-hidden shadow-md">
+<div class="sticky top-0 flex flex-col items-center justify-start flex-grow w-full overflow-hidden bg-white border-2 border-gray-300 shadow-md max-h-dvh rounded-xl">
     <div class="w-full relative pt-[calc(3/8*100%)] overflow-hidden">
-        <img class="min-h-full min-w-full object-cover absolute top-0" src="{{ $selectedJobPost->img_link }}?v={{ strtotime(date('Y-m-d H:00:00')) }}" alt="">
+        <img class="absolute top-0 object-cover min-w-full min-h-full" src="{{ $selectedJobPost->img_link }}?v={{ strtotime(date('Y-m-d H:00:00')) }}" alt="">
     </div>
-    <div class="px-5 pt-5 pb-2 flex flex-col flex-grow justify-start items-start overflow-hidden">
-        <div class="pb-5 w-full">
-            <h2 class="font-bold text-2xl">{{ $selectedJobPost->job_title }}</h2>
-            <h4 class="font-semibold text-xl">{{ $selectedJobPost->address }}</h4>
-            <h2 class="font-bold text-xl text-primary">{{ $selectedJobPost->pay }}</h2>
-            <div class="flex flex-col sm:flex-row justify-between mt-5 gap-5">
+    <div class="flex flex-col items-start justify-start flex-grow w-full px-5 pt-5 pb-2 overflow-hidden">
+        <div class="w-full pb-5">
+            <h2 class="text-2xl font-bold">{{ $selectedJobPost->job_title }}</h2>
+            <h4 class="text-xl font-semibold">{{ $selectedJobPost->address }}</h4>
+            <h2 class="text-xl font-bold text-primary">{{ $selectedJobPost->pay }}</h2>
+            <div class="flex flex-col justify-between gap-5 mt-5 sm:flex-row">
                 <p class="text-gray-500">Job ID: {{ $selectedJobPost->job_id }}</p>
                 @auth
                 @role('applicant')
@@ -17,13 +17,13 @@
                     ->exists()
                 )
                 <button 
-                    class="bg-primary text-white px-5 py-2 rounded-full opacity-75" 
+                    class="px-5 py-2 text-white rounded-full opacity-75 bg-primary" 
                     disabled
                 >Applied ✓</button>
                 @else
                 <button 
                     id="apply-now-btn" 
-                    class="bg-primary hover:opacity-75 text-white px-5 py-2 rounded-full" 
+                    class="px-5 py-2 text-white rounded-full bg-primary hover:opacity-75" 
                     data-entry-id="{{ $selectedJobPost->id }}" 
                     data-href="{{ route('applicant.get-questions', $selectedJobPost->id ) }}"
                     hx-get="{{ route('applicant.get-questions', $selectedJobPost->id ) }}"
@@ -32,7 +32,7 @@
                     hx-indicator="#apply-now-btn > .htmx-indicator"
                 >
                     Apply Now
-                    <svg class="animate-spin h-5 w-5 text-white htmx-indicator" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <svg class="w-5 h-5 text-white animate-spin htmx-indicator" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                     </svg>            
@@ -40,17 +40,17 @@
                 @endif
                 @endrole
                 @role('employer')
-                <button class="bg-primary hover:opacity-75 text-white px-5 py-2 rounded-full opacity-75" disabled>Apply Now</button>
+                <button class="px-5 py-2 text-white rounded-full opacity-75 bg-primary hover:opacity-75" disabled>Apply Now</button>
                 @endrole
                 @endauth
                 @guest
-                <a href="/login" class="bg-primary hover:opacity-75 text-white px-5 py-2 rounded-full text-center">Login to Apply</a>
+                <a href="/login" class="px-5 py-2 text-center text-white rounded-full bg-primary hover:opacity-75">Login to Apply</a>
                 @endguest
             </div>
         </div>
-        <hr class="border-t-2 border-gray-300 w-full">
-        <div class="flex-1 overflow-y-auto w-full scrollbar-thin">
-            <div class="max-h-full w-full">
+        <hr class="w-full border-t-2 border-gray-300">
+        <div class="flex-1 w-full overflow-y-auto scrollbar-thin">
+            <div class="w-full max-h-full">
                 <div class="my-5">
                     <h1 class="text-2xl font-extrabold">Job Details</h1>
                     <div class="flex flex-col gap-2 my-3">
--- resources/views/find-jobs.blade.php
+++ resources/views/find-jobs.blade.php
@@ -3,35 +3,35 @@
 <div 
     id="modal-center" 
     hx-on="htmx:afterSwap: $(this).removeClass('hidden')"
-    class="modal-center modal-applicant fixed inset-0 h-screen w-full bg-black bg-opacity-75  p-4 z-10 overflow-y-auto hidden">
+    class="fixed inset-0 z-10 hidden w-full h-screen p-4 overflow-y-auto bg-black bg-opacity-75 modal-center modal-applicant">
     
 </div>
-<div class="search-section overflow-hidden relative">
+<div class="relative overflow-hidden search-section">
     <div class="z-[-1] bg-custom-gradient-y absolute inset-0"></div>
     <img class="absolute z-[-2] min-h-full min-w-full object-cover md:translate-y-[-25%]" src="{{ asset('img/landing-page/image-no-filter.jpg') }}" alt="">
     <div class="search-section-container min-h-96 md:min-h-[500px] relative flex flex-col justify-center md:justify-end">
-        <div class="top-search-bar flex flex-col justify-end items-center px-4 md:px-5 md:mt-16 md:pb-10">
-            <form action="/find-jobs" class="search-form container max-w-screen-lg flex flex-col align-center justify-center md:mb-5" autocomplete="off">
-                <div class="form-top flex flex-col md:flex-row align-center justify-center">
+        <div class="flex flex-col items-center justify-end px-4 top-search-bar md:px-5 md:mt-16 md:pb-10">
+            <form action="/find-jobs" class="container flex flex-col justify-center max-w-screen-lg search-form align-center md:mb-5" autocomplete="off">
+                <div class="flex flex-col justify-center form-top md:flex-row align-center">
                     <select name="location" class="p-5 py-3 min-w-[300px] text-primary text-lg font-semibold rounded-t-2xl md:rounded-tr-none md:rounded-l-2xl border-none focus:outline-none focus:ring-0" name="" id="">
                         <option value="" selected>All Location</option>
                         @foreach (config('global.us_states') as $location)
                         <option value="{{ $location }}" {{ $location == $request?->location ? 'selected' : '' }}>{{ $location }}</option>
                         @endforeach
                     </select>
-                    <input name="search" value="{{ $request?->search }}" class="py-3 grow border-none text-lg focus:outline-none focus:ring-0 text-primary" type="text" placeholder="Job title or company">
-                    <div class="py-3 search-btn-wrapper px-3 rounded-b-2xl md:rounded-bl-none md:rounded-r-2xl bg-white flex items-center content-center ">
-                        <button class="bg-primary hover:opacity-75 text-white text-lg px-5 py-2 rounded-full flex-grow flex flex-row justify-center align-center gap-2" type="submit"><p>Search</p> <span class="material-symbols-outlined">search</span></button>
+                    <input name="search" value="{{ $request?->search }}" class="py-3 text-lg border-none grow focus:outline-none focus:ring-0 text-primary" type="text" placeholder="Job title or company">
+                    <div class="flex items-center content-center px-3 py-3 bg-white search-btn-wrapper rounded-b-2xl md:rounded-bl-none md:rounded-r-2xl ">
+                        <button class="flex flex-row justify-center flex-grow gap-2 px-5 py-2 text-lg text-white rounded-full bg-primary hover:opacity-75 align-center" type="submit"><p>Search</p> <span class="material-symbols-outlined">search</span></button>
                     </div>
                 </div>
             </form>
         </div>
     </div>
 </div>
-<div class="container mx-auto flex flex-col lg:flex-row lg:justify-around py-10 gap-5">
-    <div class="job-listing grow md:mx-0 flex flex-col gap-5">
+<div class="container flex flex-col gap-5 py-10 mx-auto lg:flex-row lg:justify-around">
+    <div class="flex flex-col gap-5 job-listing md:mx-0 lg:w-1/2">
         @if (!count($activeJobPosts))
-        <h3 class="text-center text-slate-500 italic">Wow, so empty... :((</h3>
+        <h3 class="italic text-center text-slate-500">Wow, so empty... :((</h3>
         @endif
         @foreach ($activeJobPosts as $jobPost)
         @include('components.find-job-page.job-posting-card', ['jobPost' => $jobPost])
@@ -40,7 +40,7 @@
     </div>
     <div 
         id="job-info-section" 
-        class="job-info-section"
+        class="job-info-section lg:w-1/2"
         hx-on="htmx:afterSwap: $('#modal-center').addClass('hidden');"
         ">
         @if (count($activeJobPosts))
