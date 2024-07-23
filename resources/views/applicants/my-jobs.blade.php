117692e79b3a64ea585c78b6f3cc2eac991358bb
Fil <filongunio@gmail.com>
2024-07-24 02:22:27 +0800

    adjust width size of job info on my jobs page

--- resources/views/applicants/my-jobs.blade.php
+++ resources/views/applicants/my-jobs.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.applicant')
 @section('content')
 <div class="container mx-auto">
-	<h1 class="text-3xl text-center font-bold my-10">My Jobs</h1>
+	<h1 class="my-10 text-3xl font-bold text-center">My Jobs</h1>
 </div>
-<div class="container mx-auto flex flex-col lg:flex-row lg:justify-around py-10 gap-5">
-  <div class="job-listing grow md:mx-0 flex flex-col gap-5">
+<div class="container flex flex-col gap-5 py-10 mx-auto lg:flex-row lg:justify-around">
+  <div class="flex flex-col gap-5 job-listing md:mx-0 lg:w-1/2">
     @if (count($myJobs))
         @foreach ($myJobs as $jobPost)
         @include('components.find-job-page.job-posting-card', ['jobPost' => $jobPost])
         @endforeach ()
         {{ $myJobs->links('vendor.pagination.custom-pagination') }}
     @else
-        <h3 class="text-center text-slate-500 italic">Wow, so empty... :((</h3>
-        <h1 class="text-center text-slate-500 italic">Click Find Jobs and start applying now</h1>
+        <h3 class="italic text-center text-slate-500">Wow, so empty... :((</h3>
+        <h1 class="italic text-center text-slate-500">Click Find Jobs and start applying now</h1>
     @endif
 
   </div>
-  <div id="job-info-section" class="job-info-section">
+  <div id="job-info-section" class="job-info-section lg:w-1/2">
       @if ($selectedJobPost)
       @include('components.find-job-page.job-info')
       @endif
